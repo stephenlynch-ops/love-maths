39b52c8a3a53b4968a77da5fddd7480a83b96ce2
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-03 09:31:20 +0000

    FEAT: Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Code that runs once the DOM content has been loaded, before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    //  let button of buttons is the modern version of a for loop with i++ etc.
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +45,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
